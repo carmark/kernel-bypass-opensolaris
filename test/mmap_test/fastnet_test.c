b65630072859f17029b3b1b799a974bb765c3586
Lei Xue <carmark.dlut@gmail.com>
2014-07-16 22:24:39 +0800

    add test files for driver

---
+++ test/mmap_test/fastnet_test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include "fastnet.h"
+
+int 
+main(int argc, char *argv[])
+{
+	int rval = 0;
+	int fd = open("/dev/fastnet", 0);
+	struct foostatus fs;
+	char * buf;
+	rval = ioctl(fd, GETSTATUS, &fs);
+
+	printf("The status from the kernel is %d\n", fs.status);
+
+	if ((buf = mmap(NULL, 4096, PROT_READ, MAP_SHARED, fd, 0)) == MAP_FAILED) {
+		printf("Can not map, the error code is : %d\n", rval);
+	} else {
+		printf("Success to map the memory, buf = %s\n", buf);
+		munmap(buf, 4096);
+	}
+	close(fd);
+	return (rval);
+}
